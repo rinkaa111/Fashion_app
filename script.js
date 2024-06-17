719e8a1b54b47af1d985a0f817e3e3791fd95276
rinkaa111 <elictrickal@gmail.com>
2024-06-17 19:14:59 +0700

    menambahkan kode untuk penyesuain angka dibelakang koma

--- script.js
+++ script.js
@@ -26,16 +26,7 @@ function showFashion(args){
                         <li class="list-group-item"><strong>A2-A3 :</strong> ${args.a2_a3()} </li>
                         <li class="list-group-item"><strong>B-B1 :</strong> ${args.b_b1()} </li>
                         <li class="list-group-item"><strong>B1-B2 :</strong> ${args.b1_b2()} </li>
-                        <li class="list-group-item"><strong>B-K1 :</strong> ${args.b_k1()}
-                        <input type="text" class="form-control" aria-label="Text input with dropdown button">
-  <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Kurus</button>
-  <ul class="dropdown-menu dropdown-menu-end">
-    <li><a class="dropdown-item" href="#">Action</a></li>
-    <li><a class="dropdown-item" href="#">Another action</a></li>
-    <li><a class="dropdown-item" href="#">Something else here</a></li>
-    <li><hr class="dropdown-divider"></li>
-    <li><a class="dropdown-item" href="#">Separated link</a></li>
-  </ul> </li>
+                        <li class="list-group-item"><strong>B-K1 :</strong> ${args.b_k1()} </li>
                         <li class="list-group-item"><strong>K-K1 :</strong> ${args.k_k1()} </li>
                         <li class="list-group-item"><strong>B-C : </strong> ${args.b_c()} </li>
                         <li class="list-group-item"><strong>C-C2 :</strong> 8 CM </li>
@@ -59,7 +50,7 @@ const method = {
         return 1.5 + ' CM';
     },
     a_a1: function(){
-        return  parseFloat((1/6 * this.leher.value)) + 0.5 + " CM";
+        return  formatNumber((1/6 * this.leher.value) + 0.5) + " CM";
     },
     a1_a2: function(){
         return this.bahu.value + " CM";
@@ -71,22 +62,40 @@ const method = {
         return  10 + " CM";
     },
     b1_b2: function(){
-        return  parseFloat((1/2 * this.l_punggung.value)) + " CM";
+        return  formatNumber((1/2 * this.l_punggung.value)) + " CM";
     },
     b_k1: function(){
         return  22 + " CM";  //20,21,22,23
     },
     k_k1: function(){
-        return  parseFloat((1/4 * this.badan.value)) -  1 + " CM";
+        return  formatNumber((1/4 * this.badan.value)) -  1 + " CM";
     },
     b_c: function(){
         return  ( this.p_punggung.value) + " CM";
     },
     c_c1: function(){
-        return  parseFloat((1/4 * this.pinggang.value)) + 0.5 + " CM";
+        return  formatNumber((1/4 * this.pinggang.value)) + 0.5 + " CM";
     },
     // a_a1: function(){
     //     return  (1/6 * this.leher.value) + 0.5 + " CM";
     // }
 }
+function formatNumber(num) {
+    // Cek apakah num adalah angka dengan desimal
+    if ((num % 1) !== 0) {
+        return parseFloat(num.toFixed(2)); // Memformat menjadi dua digit di belakang koma
+    } else {
+        return num; // Kembalikan angka asli jika tidak ada desimal
+    }
+}
 console.log(method.leher)
+
+{/* <input type="text" class="form-control" aria-label="Text input with dropdown button">
+  <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Kurus</button>
+  <ul class="dropdown-menu dropdown-menu-end">
+    <li><a class="dropdown-item" href="#">Action</a></li>
+    <li><a class="dropdown-item" href="#">Another action</a></li>
+    <li><a class="dropdown-item" href="#">Something else here</a></li>
+    <li><hr class="dropdown-divider"></li>
+    <li><a class="dropdown-item" href="#">Separated link</a></li>
+  </ul> */}
